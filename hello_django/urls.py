b8e7c0fd008972c45df10872455e5f8741a406ac
termuxuser01 <termuxuser01@gmail.com>
2023-02-01 00:22:41 -0700

    inserted app urls

--- hello_django/urls.py
+++ hello_django/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.http import JsonResponse
 from django.urls import path
+from django.url import include
 
 
-def home(request):
+def test(request):
     return JsonResponse({"hola": "como estas?"})
 
 
 urlpatterns = [
+    path('', include('base.urls'))
     path("/test", test, name="test"),
     path("admin/", admin.site.urls),
 ]
