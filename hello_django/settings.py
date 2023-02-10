91913e46e6ecf20708c51d338a4a863e0d86beee
termuxuser01 <termuxuser01@gmail.com>
2023-02-10 00:19:48 -0700

    added database url support

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -57,6 +58,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')
+}
 
 ROOT_URLCONF = 'hello_django.urls'
 
