fa6d923fe945c6da643f23dffef626370236511d
termuxuser01 <termuxuser01@gmail.com>
2023-02-09 23:46:32 -0700

    corrected static files settings

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = Path(BASE_DIR).joinpath("static")
-STATICFILES_DIRS = (Path(BASE_DIR).joinpath("staticfiles"),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
