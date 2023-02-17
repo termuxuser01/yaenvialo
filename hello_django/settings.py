7d884f07dcfddf782863d9a0d318a57755918f8c
termuxuser01 <termuxuser01@gmail.com>
2023-02-17 10:41:48 -0700

    updated setting for digital ocean static files

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -138,8 +138,8 @@ USE_TZ = True
 #     os.path.join(BASE_DIR, 'static'),
 # )
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'staticfiles')
+# ]
